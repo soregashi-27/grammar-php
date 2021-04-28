c19a3e5de1112afde77cdea81a315ec69cda4be7
soregashi-27 <tomo.soul.15@gmail.com>
2021-04-28 10:41:43 +0900

    php-training

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+echo "Good morning.\n";
+echo "Good evening.";
+echo "Good Afternoon.";
+
+$num; 
+// 変数定義
+$num = 1;
+echo $num;
+
+$num = 1;
+$num01 = 2;
+$num_01 = 3;
+// $num-01 = 3; ハイフンは使えない
+
+echo $num = 1;
+echo $num01 = 2;
+echo $num_01 = 3;
+
+
+?>
